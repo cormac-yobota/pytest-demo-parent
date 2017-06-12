f2e8d0759bfc7fadcc1b5c91caafbf8d2732d52f
Cormac Murphy <=>
2017-06-12 17:24:05 +0100

    Creating a version 0.1.0

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 import os
 from setuptools import setup
 
-DEPENDENCY_LINKS = [
-    "-e git+https://github.com/cormac-yobota/pytest-demo-parent.git@master#egg=pytest-demo-parent"
-]
 INSTALL_REQUIRES = [
-    "pytest-demo-parent",
     "pytest==3.1.1"
 ]
+VERSION="0.1.0"
 
 setup(
-    dependency_links=DEPENDENCY_LINKS,
+    version=VERSION,
     install_requires=INSTALL_REQUIRES,
     name = "pytest-demo-parent",
-    url = "https://github.com/cormac-yobota/pytest-demo-child"
+    url = "https://github.com/cormac-yobota/pytest-demo-parent"
 )
 
